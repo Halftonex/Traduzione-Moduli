eddad51db3f0824d299caafbf18b7b8f745666ff
Halftone <45943027+Halftonex@users.noreply.github.com>
2023-09-07 23:21:12 +0200

    toggle modules italian default, torch addition lights setting

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 Hooks.once('init', () => {
+    console.log("Italian Modules Translation | Inizializzazione")
     game.italianTranslations = new ItalianTranslations();
+    game.italianTranslations?.manageTranslation();
 });
 
 Hooks.once('ready', async () => {
     game.italianTranslations?.showDialog();
+    game.italianTranslations?._torchAddLights();
 });
 
 class ItalianTranslations {
@@ -11,59 +14,108 @@ class ItalianTranslations {
     constructor() {
         this.moduleId = "italian-modules-translation";
         this.showDialogSetting = "show-dialog";
+        this.overrideLang = "lang-override";
+        this.torchAddLights = "torch-lights";
         this._initSettings();
     }
 
     _initSettings() {
         game.settings.register(this.moduleId, this.showDialogSetting, {
             name: "Mostra Messaggio all'Avvio",
-            hint: "Visualizza un messaggio all'avvio del mondo con la lista dei moduli attivi di cui è presente una traduzione",
+            hint: "Visualizza un messaggio all'avvio del mondo con la lista dei moduli attivi di cui è presente una traduzione.",
             default: true,
             type: Boolean,
             scope: "world",
             config: true
         });
+        game.settings.register(this.moduleId, this.overrideLang, {
+            name: "Sovrascrivere Italiano Predefinito",
+            hint: "Sovrascrive le traduzioni in italiano già presenti nei moduli con quelle fornite da questo modulo. (Richiede ricarica del mondo)",
+            default: false,
+            type: Boolean,
+            scope: "world",
+            config: true
+        });
+        if (game.system.id === 'dnd5e' && game.modules.get("torch")) {
+            game.settings.register(this.moduleId, this.torchAddLights, {
+                name: "Torch - D&D5e Fonti di Luce Addizionali",
+                hint: "Aggiunge un file per le fonti di luce addizionali di D&D5e in italiano alle impostazioni di Torch.",
+                default: false,
+                type: Boolean,
+                scope: "world",
+                config: true
+            });
+        }
     }
 
     _isDialogEnabled() {
         return game.settings.get(this.moduleId, this.showDialogSetting);
     }
 
+    _hasItalian(languages) {
+        return languages.some(language => language.lang === 'it')
+    }    
+
     _disableDialog() {
         game.settings.set(this.moduleId, this.showDialogSetting, false)
     }
 
-    showDialog() {
-        if(game.i18n.lang === "it" && this._isDialogEnabled()) {
-            const modules = [];
-            const languages = game.modules.get(this.moduleId)?.languages;
-            for (const lang of languages) {
-                let module = game.modules.get(lang.module);
-                if(module?.active) {
-                    modules.push(module.data.title);
-                }
-            }
-            modules.sort();
+    _torchAddLights() {
+        const torch = game.modules.get("torch");
+        if (!torch || !torch.active || !game.settings.get(this.moduleId, this.torchAddLights)) return;
+        game.settings.set("torch", "gameLightSources", `modules/${this.moduleId}/lang/torch/dnd5e_fonti_di_luce.json`);        
+    }
 
-            let message = "<p>Sono state applicate le traduzioni in Italiano sui seguenti moduli attivi:</p>" +
-                "<ul style='overflow: auto; height: 300px'>";
-            for (const module of modules) {
-                message += `<li>${module}</li>`
-            }
-            message += "</ul>";
+    manageTranslation() {
+        if (game.i18n.lang !== "it") return;
 
-            new Dialog({
-                title: "Traduzione Moduli",
-                content: message,
-                buttons: {
-                    ok: { icon: '<i class="fas fa-check"></i>', label: "Ok" },
-                    dont_remind: {
-                        icon: '<i class="fas fa-times"></i>',
-                        label: "Non mostrare più",
-                        callback: this._disableDialog.bind(this)
-                    }
-                }
-            }).render(true);
+        const imt = game.modules.get(this.moduleId);
+        const { languages = [] } = imt; 
+        const ovrLanguages = [];
+
+        for (const lang of languages) {
+            const module = game.modules.get(lang.module);
+
+            if (!module?.active) continue;
+
+            if (game.settings.get(this.moduleId, this.overrideLang) || !this._hasItalian(module.languages)) {
+                ovrLanguages.push(lang);
+            }
         }
+
+        imt.languages = ovrLanguages;
+    }
+
+    showDialog() {
+        if (game.i18n.lang !== "it" || !this._isDialogEnabled()) return;
+
+        const imt = game.modules.get(this.moduleId);
+        const { languages = [] } = imt;
+
+        const modules = languages
+            .map(lang => game.modules.get(lang.module)?.title)
+            .sort();
+
+        if (modules.length === 0) return;
+
+        let message = `
+                    <p>Sono state applicate le traduzioni in Italiano sui seguenti moduli attivi:</p>
+                    <ul style='overflow: auto; height: 300px'>
+                        ${modules.map(module => `<li>${module}</li>`).join('')}
+                    </ul>
+                `;
+
+        new Dialog({
+            title: "Traduzione Moduli",
+            content: message,
+            buttons: {
+                ok: { icon: '<i class="fas fa-check"></i>', label: "Ok" },
+                dont_remind: {
+                    icon: '<i class="fas fa-times"></i>',
+                    label: "Non mostrare più",
+                    callback: this._disableDialog.bind(this)
+                }
+            }
+        }).render(true);
     }
 }
